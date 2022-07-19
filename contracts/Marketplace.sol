9b6cc11b70c925d6a51f9ab9efdc2787d4de61ef
Adebayo Temitope <adecancode@gmail.com>
2022-07-19 22:02:24 +0100

    update

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Marketplace {
+    // mint nft and set the uri
+    // sell and resell nft with royalties
+    // list nft
+    // buy nft
+    // transfer nft
+    // burn nft
+    // get nft uri
+    // get nft metadata
+    // get nft owner
+}
